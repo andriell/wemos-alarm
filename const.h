705bfd513e42db9f4d974e0980cc64cdb310fdd7
andriell <root.andriell@gmail.com>
2020-08-24 12:25:46 +0300

    v1.0.1

--- const.h
+++ const.h
@@ -1,4 +1,5 @@
 const String const_str_empty = String("");
 const String const_str_dbg = String("dbg");
 const String const_str_at = String("AT");
+const String const_str_sms = String("SMS");
 const String const_str_nr = String("\n\r");
--- sim.ino
+++ sim.ino
@@ -21,6 +21,10 @@ void simLoop() {
     simSerial->print(const_str_nr);
   }
 
+  if (cmd.startsWith(const_str_sms)) {
+    simSendSms(cmd.substring(3));
+  }
+  // Читаем ответ модуля
   int i = 0;
   while (simSerial->available()) {
     simCmdChr[i++] = simSerial->read();
@@ -36,6 +40,17 @@ void simLoop() {
   } else {
     simRespStr = const_str_empty;
   }
+}
+
+void simSendSms(String sms) {
+  simSerial->print("AT+CMGF=1");
+  simSerial->print(const_str_nr);
+  simSerial->print("AT+CMGS=\"");
+  simSerial->print(configSimPhone);
+  simSerial->print("\"");
+  simSerial->print(const_str_nr);
+  simSerial->print(sms);
+  simSerial->write(26);
 }
 
 String simResp() {
